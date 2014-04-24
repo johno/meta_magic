082b66bdda0cd959c596d1fa76f94d163bf4adfb
John Otander <johnotander@gmail.com>
2014-04-24 10:19:26 -0600

    Assign @models for collection actions.

--- lib/meta_magic/controller.rb
+++ lib/meta_magic/controller.rb
@@ -12,6 +12,10 @@ module MetaMagic
       send(:"set_#{ controller_name.classify.underscore.downcase }")
     end
 
+    def assign_models_instance_variable_array
+      instance_variable_set(:"@#{ controller_name }", get_model_class.all)
+    end
+
     def get_model_class
       controller_name.classify.constantize
     end
